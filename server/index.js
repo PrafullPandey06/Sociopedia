00cdcad1b4eb7ffa92fc76b50425bc113aad7e51
Prafull Pandey <prafullpandey62000@gmail.com>
2023-04-06 23:56:33 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
+const app = express();
+app.use(express.json());
+app.use(helmet)                                                         // to set headers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })) // browser to allow cross requests for certain resources
+app.use(morgan("common")) // give log for request and format
+app.use(bodyParser.json({ limit: "30mb", extended: true }));          // to parse json encoded data in body
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));  // parse incoming url encoded data in req body and populates it
+app.use(cors()); // without this client can't access API form different domain as of API as browser blocks it
+app.use("/assests", express.static(path.join(__dirname, ''))) // static content
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+console.log(typeof (process.env.MONGO_URL));
+const PORT = process.env.PORT || 3001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
